c22182c18592c2e23dd3b32fc6f1e51e37b21cd4
Fernanda Slomp <fernandaslomp@hotmail.com>
2024-05-15 23:28:27 -0300

    Initial commit

---
+++ src/main/java/br/com/fernandaslomp/casenotafiscalsystem/CaseNotaFiscalSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandaslomp.casenotafiscalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseNotaFiscalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseNotaFiscalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernandaslomp/casenotafiscalsystem/CaseNotaFiscalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandaslomp.casenotafiscalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseNotaFiscalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
